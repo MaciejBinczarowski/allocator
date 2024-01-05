d979409ed371c35d798335461463c4d50a25df92
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2024-01-05 23:06:30 +0100

    add headers list and browsing it befoe alloc new block

--- allocator.c
+++ allocator.c
@@ -4,11 +4,14 @@
 #define HEADER_SIZE sizeof(struct Header)
 
 void* alloc(size_t);
+void initAllocator(void);
 
 struct Header
 {
     size_t blockSize;
     int status;
+    struct Header* priorHeader;
+    struct Header* nextHeader;
 };
 
 int main(void)
@@ -17,7 +20,7 @@ int main(void)
 
     for (int i = 0; i < 10; i++)
     {
-        *(string + i + 1) = 'k'; 
+        *(string + i) = 'k'; 
     }
 
     printf("%s", string);
@@ -26,6 +29,18 @@ int main(void)
 
 void* alloc(size_t size_to_alloc)
 {
+    struct Header* currentHeader = initialHeader;
+    struct Header* priorHeader = initialHeader->priorHeader;
+    while (currentHeader != NULL)
+    {
+        if (currentHeader->status == 0 && currentHeader->blockSize > size_to_alloc)
+        {
+            // use this block
+        }
+        priorHeader = currentHeader;
+        currentHeader = currentHeader->nextHeader;
+    }
+
     void* currentBrk = sbrk(0);
     void* newBrk = sbrk(HEADER_SIZE + size_to_alloc);
 
@@ -34,9 +49,34 @@ void* alloc(size_t size_to_alloc)
         return NULL;
     }
 
-    struct Header* header = (struct Header*) currentBrk;
-    header->blockSize = size_to_alloc;
-    header->status = 1;
+    struct Header* newHeader = (struct Header*) currentBrk;
+    newHeader->blockSize = size_to_alloc;
+    newHeader->status = 1;
+    newHeader->priorHeader = priorHeader;
+    newHeader->nextHeader = NULL;
 
-    return (void*) (header + 1);
+    priorHeader->nextHeader = newHeader;
+
+    return (void*) (newHeader + 1);
+}
+
+struct Header* initialHeader = NULL;
+void initAllocator(void)
+{
+    void* startBrk = sbrk(0);
+    if(startBrk == (void*) -1)
+    {
+        // memory fault
+    }
+
+    void* newStartBrk = sbrk(HEADER_SIZE);
+    if (newStartBrk == (void*) -1)
+    {
+        //memory fault
+    }
+    
+    initialHeader = (struct Header*) startBrk;
+    initialHeader->blockSize = 0;
+    initialHeader->status = 1;
+    initialHeader->priorHeader = NULL;
 }
