2984a427f7e9c5a037e8a3dfa3a3ec2d2c64fdda
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2024-01-06 00:23:38 +0100

    complete allocation function

--- allocator.c
+++ allocator.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <cstdlib>
 
 #define HEADER_SIZE sizeof(struct Header)
+#define MIN_BLOCK_SIZE 32
 
 void* alloc(size_t);
 void initAllocator(void);
@@ -16,6 +18,7 @@ struct Header
 
 int main(void)
 {
+    initAllocator();
     char* string = (char*) alloc(10 * sizeof(char));
 
     for (int i = 0; i < 10; i++)
@@ -27,6 +30,30 @@ int main(void)
     return 0;
 }
 
+struct Header* initialHeader = NULL;
+void initAllocator(void)
+{
+    void* startBrk = sbrk(0);
+    if(startBrk == (void*) -1)
+    {
+        fprintf(stderr, "Heap error");
+        exit(EXIT_FAILURE);
+    }
+
+    void* newStartBrk = sbrk(HEADER_SIZE);
+    if (newStartBrk == (void*) -1)
+    {
+        fprintf(stderr, "Out of memory");
+        exit(EXIT_FAILURE);
+    }
+    
+    initialHeader = (struct Header*) startBrk;
+    initialHeader->blockSize = 0;
+    initialHeader->status = 1;
+    initialHeader->priorHeader = NULL;
+    initialHeader->nextHeader = NULL;
+}
+
 void* alloc(size_t size_to_alloc)
 {
     struct Header* currentHeader = initialHeader;
@@ -35,7 +62,20 @@ void* alloc(size_t size_to_alloc)
     {
         if (currentHeader->status == 0 && currentHeader->blockSize > size_to_alloc)
         {
-            // use this block
+            if (currentHeader->blockSize > size_to_alloc + HEADER_SIZE + MIN_BLOCK_SIZE)
+            {   
+                size_t secondBlockSize = currentHeader->blockSize - size_to_alloc - HEADER_SIZE;
+                struct Header* secondHeader = (struct Header*)(((char*)currentHeader) + HEADER_SIZE + size_to_alloc);
+                secondHeader->blockSize = secondBlockSize;
+                secondHeader->status = 0;
+                secondHeader->priorHeader = currentHeader;
+                secondHeader->nextHeader = currentHeader->nextHeader;
+                
+                currentHeader->blockSize = size_to_alloc;
+                currentHeader->status = 1;
+                currentHeader->nextHeader = secondHeader;
+            }
+            return (void*) (currentHeader + 1);
         }
         priorHeader = currentHeader;
         currentHeader = currentHeader->nextHeader;
@@ -58,25 +98,4 @@ void* alloc(size_t size_to_alloc)
     priorHeader->nextHeader = newHeader;
 
     return (void*) (newHeader + 1);
-}
-
-struct Header* initialHeader = NULL;
-void initAllocator(void)
-{
-    void* startBrk = sbrk(0);
-    if(startBrk == (void*) -1)
-    {
-        // memory fault
-    }
-
-    void* newStartBrk = sbrk(HEADER_SIZE);
-    if (newStartBrk == (void*) -1)
-    {
-        //memory fault
-    }
-    
-    initialHeader = (struct Header*) startBrk;
-    initialHeader->blockSize = 0;
-    initialHeader->status = 1;
-    initialHeader->priorHeader = NULL;
 }
