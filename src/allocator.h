d66b19e295436837f385ccea477100921b766214
Maciej Binczarowski <maciej.binczarowski@gmail.com>
2024-01-14 22:38:48 +0100

    comments

--- src/allocator.h
+++ src/allocator.h
@@ -3,6 +3,7 @@
 #ifndef ALLOCATOR_H
 #define ALLOCATOR_H
 
+// struct for collecting statistics
 struct AllocatorStatistics {
     size_t totalAllocatedBytes;
     size_t currentAllocatedBytes;
@@ -14,11 +15,20 @@ struct AllocatorStatistics {
     int allocatedBlocksCount;
 };
 
+// returns struct with stats
 struct AllocatorStatistics getAllocatorStatistics(void);
 
+// it works with macro alloc(size_to_alloc)
+// alligns block size to header size
 void* allocate(size_t, const char*, int);
+
+// simply dealocs blocks and merge with free neighboors
 void dealloc(void*);
+
+// memory dump
 void printBlockList(void);
+
+// function to initialize allocator
 void initAllocator(void);
 
 #define alloc(size_to_alloc) allocate(size_to_alloc, __FILE__, __LINE__)
